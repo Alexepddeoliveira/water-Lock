6b0fa0f6b2a2e4d8f72b38cdb4b18a7c789de6ff
Alex euzebio pereira dias de oliveira <127499015+Alexepddeoliveira@users.noreply.github.com>
2024-10-22 11:37:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 document.getElementById("formContato").addEventListener("submit", function(event){
     event.preventDefault();
-    alert("Mensagem enviada com sucesso!");
+    alert("Sua mensagem foi enviada com sucesso!");
 });
